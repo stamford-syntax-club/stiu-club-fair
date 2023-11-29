18a7ad870fa64762a5c1eeb7cda347724f7a2680
Lxkas <alohasunshineday@yahoo.com>
2023-11-29 21:06:54 +0700

    add P E R S P E C T I V E

---
+++ perspective.js
@@ -0,0 +1,16 @@
+// script.js
+document.addEventListener("mousemove", (e) => {
+	const name = document.getElementById("tawan_name");
+	const { clientX, clientY } = e;
+
+	const centerX = window.innerWidth / 2;
+	const centerY = window.innerHeight / 2;
+
+	const deltaX = clientX - centerX;
+	const deltaY = clientY - centerY;
+
+	const rotateY = (deltaX / centerX) * 60; // adjust for sensitivity
+	const rotateX = (-deltaY / centerY) * 60; // adjust for sensitivity
+
+	name.style.transform = `rotate3d(${rotateX}, ${rotateY}, 0, ${Math.sqrt(rotateX * rotateX + rotateY * rotateY)}deg)`;
+});
